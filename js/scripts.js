b3c633e529fde6a1f98eba4f4807a9c981c2472b
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-30 15:12:32 -0700

    tell the user wht flavors are avaliable

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   $("form#flavor").submit(function(event){
     event.preventDefault();
-    var flavors = ['Pistachio', 'Peppermint', 'Chocolate Peanut Butter', 'Cookies & Cream']
-    flavors.forEach(function(flavors) {
-      console.log(flavors)
+    var flavors = ['Pistachio', 'Peppermint', 'Chocolate Peanut Butter', 'Cookies & Cream'];
+    flavors.forEach(function(flavor) {
+      $('.output').append("<li>" + flavor + "</li>")
+
+      //$(flavors).show();
+      //return(flavors);
+      //alert('My favorite flavor of icecream is' + " " + flavors);
+      //var flavors = $("flavors#" + flavor).val();
+      //$("." + flavor).text(flavors);
+      //console.log(flavors)
+      //$("#Pistachio", "Peppermint").text(flavors[0, 1, 2, 3])
+      //console.log(flavors)
+      //$("#input").text(flavors[0, 1, 2, 3]);
+   
     });
   });
 });
