1dd8b5612219ec5b2082a944a2534edb1a7c0c91
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-30 14:05:42 -0700

    flavors interact from js to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("form#flavor").submit(function(event){
     event.preventDefault();
+    var flavors = ['Pistachio', 'Peppermint', 'Chocolate Peanut Butter', 'Cookies & Cream']
+    flavors.forEach(function(flavors) {
+      console.log(flavors)
+    });
   });
 });
